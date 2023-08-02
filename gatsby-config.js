505efdb90f99026d121afbc07b5ae69d4b3ed5f5
taemin-Jang <jtm990506@gmail.com>
2023-08-02 17:25:01 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `taemin-jang.github.io`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
