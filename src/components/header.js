17591634d9c485151e833adf5b90f1f523ecda68
taemin-Jang <jtm990506@gmail.com>
2023-08-06 22:07:08 +0900

    Modify header list, logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { StaticImage } from "gatsby-plugin-image";
 import { Link, useStaticQuery, graphql } from "gatsby";
 const Header = () => {
   const data = useStaticQuery(graphql`
@@ -11,19 +10,11 @@ const Header = () => {
       }
     }
   `);
-  console.log("data", data);
   return (
     <header>
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <Link to="/" className="flex items-center">
-            <StaticImage
-              src="../images/icon.png"
-              alt="Flowbite Logo"
-              width={50}
-              height={50}
-              className="mr-2"
-            />
+          <Link to="/" className="flex items-center p-4">
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
               {data.site.siteMetadata.title}
             </span>
@@ -35,7 +26,7 @@ const Header = () => {
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
                 <Link
-                  href="#"
+                  href="#home"
                   className="block py-2 pr-4 pl-3 text-white rounded bg-primary-green lg:bg-transparent lg:text-primary-green lg:p-0 dark:text-white"
                   aria-current="page"
                 >
@@ -44,59 +35,23 @@ const Header = () => {
               </li>
               <li>
                 <Link
-                  href="#"
+                  href="#skills"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-green_hover lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Company
+                  Skills
                 </Link>
               </li>
               <li>
                 <Link
-                  href="#"
+                  href="#project"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-green_hover lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Marketplace
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="#"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-green_hover lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Features
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="#"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-green_hover lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Team
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="#"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-green_hover lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Contact
+                  Project
                 </Link>
               </li>
             </ul>
           </div>
           <div className="flex items-center lg:order-2">
-            <Link
-              href="#"
-              className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-            >
-              Log in
-            </Link>
-            <Link
-              href="#"
-              className="text-white bg-primary-green hover:bg-primary-green_hover focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-green_hover focus:outline-none dark:focus:ring-primary-800"
-            >
-              Get started
-            </Link>
             <button
               data-collapse-toggle="mobile-menu-2"
               type="button"
