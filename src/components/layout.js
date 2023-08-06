2f20511ed82564f2d78d4a075fa36354ae301983
taemin-Jang <jtm990506@gmail.com>
2023-08-06 22:13:44 +0900

    Modfiy layout and a tag move slowly

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,19 +2,20 @@ import * as React from "react";
 import Header from "./header";
 import Hero from "./hero";
 import Tech from "./tech";
-import Project from "./project";
-
+import Project1 from "./projects/project1";
+import Project2 from "./projects/project2";
+import Project3 from "./projects/project3";
+import Project4 from "./projects/project4";
 const Layout = () => {
   return (
     <div className="layout">
       <Header />
       <Hero></Hero>
       <Tech></Tech>
-      <Project></Project>
-      {/* <footer>
-        <p>Copyright 2023</p>
-        <p>All rights reserved.</p>
-      </footer> */}
+      <Project4></Project4>
+      <Project3></Project3>
+      <Project2></Project2>
+      <Project1></Project1>
     </div>
   );
 };
